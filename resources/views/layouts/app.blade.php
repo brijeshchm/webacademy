3d65468393eb6cdc850a53d8c59acf9c87b68a70
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-08-22 14:11:18 +0530

    verficate

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,14 +33,36 @@
 <meta name="twitter:description" content="@yield('description')">
 <meta name="twitter:image" content="@yield('og_image', asset('images/logo-academy.webp'))">
 <meta name="twitter:image:alt" content="@yield('title')">
+<meta name="google-site-verification" content="cdZRjAwe6T9AoQoE-gqxeeMPgs_8FuKWR3GXR2JJI2c" />
 <link rel="icon" href="/favicon.ico">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/app.css">  
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PVZ24G2F');</script>
+<!-- End Google Tag Manager -->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RX53MZVN0M"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RX53MZVN0M');
+</script>
 @stack('schema') 
 </head>
-<body>
+<body> 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVZ24G2F"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
 <div class="flex flex-col min-h-screen bg-background text-foreground transition-colors duration-300">
 @include('partials.navbar')
 <main class="flex-1 mt-[72px]">
