bfd554979c976b9a60c51305f8ca25a1136d4ffb
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-08-25 19:04:22 +0530

    site

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -385,10 +385,16 @@ class CourseController extends Controller implements HasMiddleware
 				return response()->json(['status'=>1,'errors'=>$errorsBag],400);
 			}
 				  
-			$updatelearn =array(
-			'trainer_about'=>$request->input('trainer_about'),	
-			'trainer_paragraph'=>json_encode($request->input('trainer_paragraph')));	
+			
+			$updatelearn = array(
+    'trainer_about' => $request->input('trainer_about'),
+);
 
+if ($request->filled('trainer_paragraph')) {
+    $updatelearn['trainer_paragraph'] = json_encode(
+        $request->input('trainer_paragraph')
+    );
+}
 			$checkupdate  =DB::table('web_courses')->where('id',$id)->update($updatelearn);	
 			if($checkupdate){
 				$status=1;							 
